93751993413e2c8afb6636cb862fba9e5c617684
Jenn <solvcreative@gmail.com>
2021-03-04 12:34:45 -0600

    Completed exercise

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Testing.Models;
 
 namespace Testing.Controllers
 {
@@ -15,11 +16,59 @@ namespace Testing.Controllers
             this.repo = repo;
         }
         
-        // GET: /<controller>/
+        // GET ALL: /<controller>/
         public IActionResult Index()
         {
             var products = repo.GetAllProducts();
-            return View();
+            return View(products);
         }
+
+        // GET 1 product: /<controller>/
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }
+
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+
+        public IActionResult InsertProduct()
+        {
+            var prod = repo.AssignCategory();
+
+            return View(prod);
+        }
+
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            repo.InsertProduct(productToInsert);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -12,15 +12,67 @@ namespace Testing
     {
        
         private readonly IDbConnection _conn;
+     
         public ProductRepository(IDbConnection conn)
         {
             _conn = conn;
         }
-
+        //getting all products
         public IEnumerable<Product> GetAllProducts()
         {
             return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
         }
+
+        //getting single product using QuerySingle<Product> Dapper method to return single row
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+                new { id = id });
+        }
+        
+        //adding ability to update the product
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+                new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
+
+        //adding ability to insert a new product
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+
+        //adding ability to get categories
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+
+        //adding ability to assign category
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+
+            return product;
+        }
+
+        //ability to delete products
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+        }
+
+
     }
+
 
 }
